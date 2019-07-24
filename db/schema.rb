017c6a48a480f8747de2a3120a1e924904b5a150
Tiago Garcia <tiago.tiago22@gmail.com>
2019-07-24 00:20:33 -0300

    Add missing configuration at tag table

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2019_07_23_044227) do
   end
 
   create_table "tags", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
