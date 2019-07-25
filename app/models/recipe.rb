1fe1b9fd123fc9656e71ca17df37a74c36d7aa14
Tiago Garcia <tiago.tiago22@gmail.com>
2019-07-25 01:55:59 -0300

    Add support to nested attributes creation at Recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
 
   belongs_to :chef, optional: true
   has_and_belongs_to_many :tags
+
+  accepts_nested_attributes_for :tags, reject_if: proc { |tag| tag['name'].blank? }
+  accepts_nested_attributes_for :chef, reject_if: proc { |tag| tag['name'].blank? }
 end
