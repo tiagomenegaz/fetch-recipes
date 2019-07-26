ca158744addc46483884f4bafe2a3a6a295e651e
Tiago Garcia <tiago.tiago22@gmail.com>
2019-07-26 02:10:19 -0300

    Update show controller to avoid extra queries

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -4,7 +4,7 @@ class RecipesController < ApplicationController
   end
 
   def show
-    @recipe = Recipe.includes(:chef, :tags).find_by(id: params[:id])
+    @recipe = Recipe.eager_load(:chef, :tags).find_by(id: params[:id])
     redirect_to root_path if @recipe.nil?
   end
 end
